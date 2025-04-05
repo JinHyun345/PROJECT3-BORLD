ca31a9f83220a29710725e320db455c460f0395d
muamyo <kimjh0957@gmail.com>
2025-04-05 20:25:23 +0900

    fix: fix db와 env railway값으로

--- back/models/db.js
+++ back/models/db.js
@@ -1,12 +1,11 @@
-const mysql = require("mysql2");
-require("dotenv").config();
+import mysql from 'mysql2';
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
-module.exports = db;
+export default db;
