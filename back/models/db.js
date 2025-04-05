937b1b8f8b7c7d3e8ad310b0f427e12ae45bd8af
muamyo <kimjh0957@gmail.com>
2025-04-05 18:27:58 +0900

    feat: DB 연결 환경변수로 변경

--- back/models/db.js
+++ back/models/db.js
@@ -1,10 +1,14 @@
 import mysql from 'mysql2';
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
+  host: process.env.DB_HOST,       // ex) 'aws.connect.psdb.io'
+  user: process.env.DB_USER,       // ex) 'admin'
   password: process.env.DB_PASSWORD,
-  database: "p3"
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306, // 보통 기본 포트지만 명시해주는 게 좋아
+  ssl: {
+    rejectUnauthorized: true,      // PlanetScale 등 일부 서비스에서 필요
+  }
 });
 
 export default db;
