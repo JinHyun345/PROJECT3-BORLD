3369bff2bfbff4316a8038eae76208ca47291843
muamyo <kimjh0957@gmail.com>
2025-04-05 18:03:55 +0900

    fix: server.js Render배포위해 수정

--- back/server.js
+++ back/server.js
@@ -1,6 +1,5 @@
 import app from './app.js';
-const port = 5000;
-
-app.listen(port, () => {
-  console.log(`✅ 서버 실행 중! 포트: ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`✅ 서버 실행 중! 포트는 ${PORT}`);
 });
