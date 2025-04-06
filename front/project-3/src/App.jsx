5b2f41285787ee1a5916716f50dda08a2ebedb46
muamyo <kimjh0957@gmail.com>
2025-04-06 17:00:36 +0900

    오늘하루도 끝

--- front/project-3/src/App.jsx
+++ front/project-3/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 const apiUrl = import.meta.env.VITE_API_URL;
-console.log("API URL:", apiUrl);
 
 const usePreventBackNavigation = () => {
   useEffect(() => {
